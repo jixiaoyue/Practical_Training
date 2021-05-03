18aaf07c5762cdca0481fee81c16e82c65e38898
jixiaoyue <1250820271@qq.com>
2021-05-03 14:57:22 +0800

    monkey命令学习汇汇总

---
+++ project/monkey命令汇总/bat/monkey-homeworkteacher.bat
@@ -0,0 +1,2 @@
+adb shell monkey -p com.example.homeworkcorrectteacher -s 1234 --throttle 200 -v -v -v 1000 >e:\demo\mongkey0503\m2.log
+pause
---
+++ project/monkey命令汇总/bat/yeshen.bat
@@ -0,0 +1,4 @@
+d:
+cd  D:\Program Files\Nox\bin
+nox_adb.exe connect 127.0.0.1:52001
+pause
